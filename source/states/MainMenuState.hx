3147c111b24305e170760fe1720c73fa0a8b3110
Almo36 <mealmohammad0@gmail.com>
2024-11-26 21:24:08 +0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -40,7 +40,7 @@ class MainMenuState extends FlxState {
         add(creditsButton);
     }
 }
-
+{
 	public static var psychEngineVersion:String = '1.0'; // This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var curColumn:MainMenuColumn = CENTER;
